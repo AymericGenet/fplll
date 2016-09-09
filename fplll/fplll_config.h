867b4d9cfab81e658d63fcdead7abec68b17f67c
Genet Aymeric <aymeric.gen@gmail.com>
2016-09-09 08:51:03 +0200

    Add version 5.0.3rc0 of config headers.

---
+++ fplll/fplll_config.h
@@ -0,0 +1,29 @@
+/* fplll/fplll_config.h.  Generated from fplll_config.h.in by configure.  */
+#ifndef FPLLL_CONFIG__H
+#define FPLLL_CONFIG__H
+
+/* use quaddouble library */
+/* #undef FPLLL_WITH_QD */
+
+/* fplll major version */
+#define FPLLL_MAJOR_VERSION 5
+
+/* fplll minor version */
+#define FPLLL_MINOR_VERSION 0
+
+/* fplll micro version */
+#define FPLLL_MICRO_VERSION 3rc0
+
+/* fplll sonum */
+#define FPLLL_SONUM
+
+/* fplll version */
+#define FPLLL_VERSION 5.0.3rc0
+
+/* fplll version info */
+#define FPLLL_VERSION_INFO 
+
+/* Maximum supported enumeration dimension */
+#define FPLLL_MAX_ENUM_DIMENSION 256
+
+#endif //FPLLL_CONFIG__H
